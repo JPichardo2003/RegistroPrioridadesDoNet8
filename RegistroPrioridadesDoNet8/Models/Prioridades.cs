c3cd2292139cb0c14ca0a654b65b3aaa5a7e041f
JPichardo2003 <julio_pichardo@ucne.edu.do>
2024-01-10 22:33:02 -0400

    Añadiendo el Modelo Prioridades

---
+++ RegistroPrioridadesDoNet8/Models/Prioridades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroPrioridadesDoNet8.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Descripción { get; set; }
+        [Range(1, 31, ErrorMessage = "Valores Validos entre 1 y 31")]
+        public int DiasCompromiso { get; set; }
+    }
+}
